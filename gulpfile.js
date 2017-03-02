6d790425590a86ac76f2ff95bcc8186ea78b3817
kon <kon@vsoloop.com>
2017-03-02 20:10:45 +0800

    [Update] update gulp files

--- gulpfile.js
+++ gulpfile.js
@@ -41,7 +41,7 @@ var inject = require('gulp-inject'),
 
 
 // //////////////////////////////
-// Settings
+// Config Settings
 // //////////////////////////////
 var g_app = {
   scss : ['src/scss/**/*.scss', 'src/css/**/*.css'],
@@ -100,19 +100,17 @@ gulp.task('iconfont', function(){
     .pipe(iconfont({
       fontName: 'iconFont', // required
       //prependUnicode: true, // recommended option
-
       formats: ['ttf', 'eot', 'woff', 'woff2'],
       appendCodepoints: true,
       appendUnicode: false,
       normalize: true,
       fontHeight: 1000,
       centerHorizontally: true
-
       //formats: ['ttf', 'eot', 'woff'], // default, 'woff2' and 'svg' are available
       //timestamp: runTimestamp, // recommended to get consistent builds when watching files
     }))
       .on('glyphs', function(glyphs, options) {
-        console.log(glyphs, options);
+        //console.log(glyphs, options);
       })
     .pipe(gulp.dest('dev/fonts/icon'));
 });
